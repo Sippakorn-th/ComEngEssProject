5b2762c2fd74214f1ff0fcba0f513bf4b17e7cdb
Pisitpong Kreangkrailipikon <kreangkrailipikonp@gmail.com>
2024-04-19 13:11:20 +0700

    implement navigate to all pages

--- frontend/public/scripts/navigate.js
+++ frontend/public/scripts/navigate.js
@@ -2,3 +2,28 @@ function goToMainPage() {
   console.log("HI");
   window.location.href = "./mainpage.html";
 }
+
+function goToIndexPage() {
+  console.log("HI");
+  window.location.href = "./index.html";
+}
+function goToLeaderboardPage() {
+  console.log("HI");
+  window.location.href = "./leaderboard.html";
+}
+function goToGamePlayPage() {
+  console.log("HI");
+  window.location.href = "./gameplaypage.html";
+}
+function goToProfilePage() {
+  console.log("HI");
+  window.location.href = "./profilepage.html";
+}
+function goToSignInPage() {
+  console.log("HI");
+  window.location.href = "./signinpage.html";
+}
+function goToSignUpPage() {
+  console.log("HI");
+  window.location.href = "./signuppage.html";
+}
